4c4717f3870faa9bfdadc7167a0434a9ca51e215
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2025-08-07 09:09:39 +0200

    upgrade all versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "2.1.20"
+    id("org.jetbrains.kotlin.jvm") version "2.2.0"
     application
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.20"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.0"
     alias(libs.plugins.shadow)
     alias(libs.plugins.versions)
 }
